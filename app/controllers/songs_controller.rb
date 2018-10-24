dadffe0d825b8918c3ee221e5c7cceaa5f868573
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-24 18:16:50 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,21 +1,27 @@
 class SongsController < ApplicationController
   
   def new
-    
+    @song = Song.new
+  end
+  
+  def show
+    @song = Song.find(params[:id])
   end
   
   def create
+    @song = Artist.new(params.require(:artist).permit(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
   end
 
-  def edit
-  end
-
-  def show
-    
+  def edit 
+    @artist = Artist.find(params[:id])
   end
   
   def update
-    
+    @artist = Artist.find(params[:id])
+    @artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
   end
   
 end
