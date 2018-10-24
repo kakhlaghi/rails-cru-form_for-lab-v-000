e4bb1b8469ecf42645b526e3e2e48de94186a260
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-24 15:16:32 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,19 +1,25 @@
 class GenresController < ApplicationController
   
-  def new 
-    
+def new
+    @genre = Genre.new
+  end
+  
+  def show
+    @Genre = Genre.find(params[:id])
   end
   
   def create
+    @genre = Genre.new(params.require(:genre).permit(:name, :bio))
+    @genre.save
+    redirect_to genre_path(@genre)
   end
 
-  def edit
-  end
-
-  def show
+  def edit 
+    @artist = Artist.find(params[:id])
   end
   
   def update
-    
+    @artist = Artist.find(params[:id])
+    @artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
   end
-end
